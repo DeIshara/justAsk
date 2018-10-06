71c5b79ca2a742bcf7a9423e7d9acbae705fa3c4
ashan1995 <ashan.samarasinghe95@gmail.com>
2018-10-06 11:27:39 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php   
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="CSS/index.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row logopanel">
+            <div class="col-md-3">
+                <img id="logo" src="" alt="">
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3">
+                <button class="btn-primary loginbtn">Log In</button>
+            </div>
+        </div>
+        <div class="row">
+            
+            <div class="custom-jumbotron">
+          
+
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="info">
+                        <h1>Just Ask</h1>
+                        <p>Just ask for a ride!!!!</p>
+                        <p>
+                            <a class="btn btn-primary btn-lg">Ask Now</a>
+                        </p>
+                    </div>
+                   
+                </div>
+                <div class="col-md-4"></div>
+                   
+               
+            </div>
+               
+            
+        </div>
+        <div class="row aboutus">
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+        </div>
+
+        <div class="row foot">
+          
+        </div>
+    </div>
+</body>
+</html>
